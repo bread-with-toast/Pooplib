564f501f0b65f1fb0813f70e90a637d611567a31
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-07-29 19:07:19 +0100

    Update maths.lua

--- samples/maths.lua
+++ samples/maths.lua
@@ -1,3 +1,5 @@
-set_pooparle("Num1", 3)
-set_pooparle("Num2", 3)
-poop_text(pooparles["Num1"] .. " + " .. pooparles["Num2"] .. " = " .. pooparles["Num1"] + pooparles["Num2"])
+M = require("main")
+
+M.set_pooparle("Num1", 3)
+M.set_pooparle("Num2", 3)
+M.poop_text(pooparles["Num1"] .. " + " .. pooparles["Num2"] .. " = " .. pooparles["Num1"] + pooparles["Num2"])
